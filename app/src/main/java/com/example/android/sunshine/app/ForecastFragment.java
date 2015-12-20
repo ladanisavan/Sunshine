f7674f2455c7faa83cc0b19ec955d83d9739485b
Savan Ladani <savan.ladani@gmail.com>
2015-12-20 19:53:37 +0530

    add toast

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -99,7 +101,12 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView)rootView.findViewById(
                 R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), forecastAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
 
